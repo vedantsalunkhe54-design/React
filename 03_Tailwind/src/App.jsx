f132a9f9e368941dc62b5b40433fae4d8616ef72
Vedant Salunkhe <vedantsalunkhe54@gmail.com>
2026-05-04 14:02:18 +0000

    Tailwind and Props

---
+++ 03_Tailwind/src/App.jsx
@@ -0,0 +1,23 @@
+import Cards from "./Components/Cards"
+
+function App() {
+
+
+  return (
+    <div >
+      <h1 className="bg-blue-800 text-yellow-400 px-4 py-6 inline-block rounded-xl">
+        COLOR TEST
+      </h1>
+
+      <Cards username="Vedant" quote="Never Give Up"/>
+      <Cards username="Salunkhe" quote="I Always Win"/>
+
+    </div>
+  )
+}
+
+export default App
+
+
+
+
